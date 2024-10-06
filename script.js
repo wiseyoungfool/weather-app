ea0782f84b4013b4ac3dc8a2290d7ef91dfc101f
wiseyoungfool <ninjaclippings@gmail.com>
2024-10-06 12:13:22 -0400

    Add icons and improve styling

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const searchButton = document.getElementById("get-weather");
 const weather = document.getElementById("weather");
 const loc = document.getElementById("location");
 const temp = document.getElementById("temp");
+const icon = document.getElementById("main-weather-icon");
 
 // Details
 const description = document.getElementById("description");
@@ -16,7 +17,7 @@ const humidity = document.getElementById("humidity");
 const precipitationProbability = document.getElementById("prec-prob");
 
 searchForm.addEventListener("submit", searchWeather);
-//getWeather("Philadelphia");
+getWeather("Philadelphia");
 
 async function getWeather(location) {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=4EJ7R4VXQT4N3V8P4Y2D2D5QA`, {mode: 'cors'});
@@ -34,9 +35,24 @@ async function getWeather(location) {
             feelsLike.textContent = "Feels Like: " + response.currentConditions.feelslike + "°F";
             humidity.textContent = "Humidity: " + response.currentConditions.humidity + "%";
             wind.textContent = "Wind: " + response.currentConditions.windspeed + "mph";
+
+            setWeatherIcon(response.currentConditions.icon);
         });
     }
 
+}
+
+function setWeatherIcon(iconName) {
+    const iconPath = `icons/${iconName}.svg`;
+    const weatherIconDiv = document.getElementById('main-weather-icon');
+    weatherIconDiv.innerHTML = '';
+
+    const imgElement = document.createElement('img');
+    imgElement.src = iconPath;
+    imgElement.alt = iconName;
+
+    weatherIconDiv.appendChild(imgElement);
+    console.log(`Weather Icon Set: ${iconName}`);
 }
 
 function searchWeather() {
