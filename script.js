e41c474f7b78141c5549ae876e156aa9f88551d9
wiseyoungfool <ninjaclippings@gmail.com>
2024-10-07 15:59:08 -0400

    Add metric system toggle button

--- script.js
+++ script.js
@@ -16,9 +16,18 @@ const feelsLike = document.getElementById("feels-like");
 const wind = document.getElementById("wind");
 const humidity = document.getElementById("humidity");
 const precipitationProbability = document.getElementById("prec-prob");
+const useCelciusCheck = document.getElementById("celcius-button");
+const useCelciusButton = document.getElementById("metric-system");
+
+let useCelcius = false;
+let currentCity = "Philadelphia";
+useCelciusCheck.checked=useCelcius;
 
 searchForm.addEventListener("submit", searchWeather);
-getWeather("Philadelphia");
+//useCelciusCheck.addEventListener("click", toggleCelcius);
+useCelciusButton.addEventListener("click", toggleCelcius);
+
+//getWeather("Philadelphia");
 
 async function getWeather(location) {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=4EJ7R4VXQT4N3V8P4Y2D2D5QA`, {mode: 'cors'});
@@ -29,14 +38,21 @@ async function getWeather(location) {
     else {
         response.json().then(function(response) {
             console.log(response);
+            currentCity = location;
             loc.textContent = response.resolvedAddress;
             weather.textContent = response.currentConditions.conditions;
-            temp.textContent = response.currentConditions.temp + "°F";
-            description.textContent = response.description;
-            feelsLike.textContent = "Feels Like: " + response.currentConditions.feelslike + "°F";
-            humidity.textContent = "Humidity: " + response.currentConditions.humidity + "%";
-            wind.textContent = "Wind: " + response.currentConditions.windspeed + "mph";
 
+            if (useCelcius===false) temp.textContent = response.currentConditions.temp + "°F";
+            else temp.textContent = ((response.currentConditions.feelslike - 32) * (5/9)).toFixed(1) + "°C";
+
+            description.textContent = response.description;
+
+            if (useCelcius===false) feelsLike.textContent = "Feels Like: " + response.currentConditions.feelslike + "°F";
+            else feelsLike.textContent = "Feels Like: " + ((response.currentConditions.feelslike - 32) * (5/9)).toFixed(1) + "°C";
+
+            humidity.textContent = "Humidity: " + response.currentConditions.humidity + "%";
+            if (useCelcius===false) wind.textContent = "Wind: " + response.currentConditions.windspeed + "mph";
+            else wind.textContent = "Wind: " + (response.currentConditions.windspeed*1.609344).toFixed(1) + "kph";
             setWeatherIcon(response.currentConditions.icon);
             populateForecast(response)
         });
@@ -65,11 +81,13 @@ function populateForecast(response) {
         day.appendChild(condition);
 
         const temp = document.createElement("div");
-        temp.textContent = dayData.temp + '°';
+        if (useCelcius===false) temp.textContent = dayData.temp + '°F';
+        else temp.textContent = ((dayData.temp - 32) * (5/9)).toFixed(1) + "°C";
         day.appendChild(temp);
 
         const highLow = document.createElement("div");
-        highLow.textContent = dayData.tempmax + "°/" + dayData.tempmin + "°";
+        if (useCelcius===false) highLow.textContent = dayData.tempmax + "°F/" + dayData.tempmin + "°F";
+        else highLow.textContent = ((dayData.tempmax - 32) * (5/9)).toFixed(1) + "°C/" + ((dayData.tempmin - 32) * (5/9)).toFixed(1) + "°C";
         day.appendChild(highLow);
 
         const prec = document.createElement("div");
@@ -106,4 +124,13 @@ function searchWeather() {
     const location = searchBox.value;
     searchBox.value = "";
     getWeather(location);
+}
+
+function toggleCelcius() {
+    event.preventDefault();
+    useCelcius = !useCelcius;
+    console.log("Use Celcius: ", useCelcius);
+
+    useCelciusCheck.checked=useCelcius;
+    getWeather(currentCity);
 }
